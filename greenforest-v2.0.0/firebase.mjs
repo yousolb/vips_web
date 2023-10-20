a0c88a777d98e6abdabde6225188b2820fb68593
yousolb <yousol@umich.edu>
2023-10-20 18:20:20 -0400

    firebase.mjs done

--- greenforest-v2.0.0/firebase.mjs
+++ greenforest-v2.0.0/firebase.mjs
@@ -21,25 +21,19 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 
-let fires = getFirestore(app);
-let col = collection(fires, 'products');
-let qs = await getDocs(col);
-let docu = qs.docs[0];
-//console.log(docu.data());
-let product_data = docu.data()
-
-let storage = getStorage(app);
-let reference = ref(storage, product_data.image_folder);
+export let fires = getFirestore(app);
+export let col = collection(fires, 'products');
+export let storage = getStorage(app);
 
 
-function setHTML(selector, string) {
+export function setHTML(selector, string) {
   let elts = document.querySelectorAll(selector);
   for (let i=0; i < elts.length; i++) {
     elts[i].innerHTML = string;
   }
 }
 
-async function setSRC(selector, item) {
+export async function setSRC(selector, item) {
   let elts = document.querySelectorAll(selector);
   let url = await getDownloadURL(item);
   for (let i=0; i < elts.length; i++) {
