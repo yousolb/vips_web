ee896ace5057e7e8166feef179a5f96309b6342c
yousolb <yousol@umich.edu>
2024-05-01 20:45:09 -0400

    new about page, changed volunteer/contact page, changed header, new projects page

--- greenforest-v2.0.0/team.mjs
+++ greenforest-v2.0.0/team.mjs
@@ -8,20 +8,22 @@ let elt = document.querySelector(`.staff-items`);
 
 for(let i = 0; i < docus.length; i++) {
     let doc = docus[i];
-    let product_data = doc.data();
-    let reference = ref(storage, product_data.image);
+    let artist_data = doc.data();
+    let reference = ref(storage, artist_data.image);
     let url = await getDownloadURL(reference);
-    let prod_id = doc.id;
-    console.log(product_data.name)
+    let artist_id = doc.id;
+    console.log(artist_data.name)
 
-    let collection_items = `<div class="col-lg-3 col-sm-6 col-12">
+    let collection_items = `<div>
     <div class="volunteers-items">
         <div class="volunteers-img">
             <img src="${url}" alt="volunteers-img-1" class="img-responsive" />
         </div>
         <div class="volunteers-content">
-            <h4><a href="team_single.html?prodid=${prod_id}">${product_data.name}</a></h4>
-            <p>${product_data.label}</p>
+            <h4>${artist_data.name}</h4>
+            <i>${artist_data.label}</i>
+            <p>Favorite animal: ${artist_data.fav_animal}</p>
+            <p>${artist_data.description}</p>
         </div>
     </div>
 </div>`
