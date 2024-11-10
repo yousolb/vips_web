94f5ad2ec7e698b3e8eac38ea01fb2d1c16db6da
Baksu888 <vickeyli@umich.edu>
2024-11-10 15:12:42 -0500

    Add part of donate functionality

--- greenforest-v2.0.0/donate.js
+++ greenforest-v2.0.0/donate.js
@@ -1,6 +1,23 @@
 // This is your public API key.
 const stripe = Stripe("pk_live_51OtTziFV91ot5Yz84YA5iMOCaOMRZEIPIex8VBFEbooI2bSb30eExeNyzVRwIarhMNr6ErUKv0R7FULyNYToQfEp00VHT7KM44");
 
+const donateOptions = {
+  "donate-once-1": {
+    price: "price_1QIHS0FV91ot5Yz8upssMn0D",
+    quantity: 1
+  },
+  "donate-monthly-custom": {
+    price: "price_1QAg1cFV91ot5Yz8Q32xYYTg",
+    quntity: 1
+  }
+}
+
+const params = new URLSearchParams(window.location.search)
+const option = params.get("val")
+console.log(option)
+
+initialize();
+
 // Create a Checkout Session
 async function initialize() {
   const fetchClientSecret = async () => {
@@ -10,7 +27,9 @@ async function initialize() {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        items: [1,2,3,4],
+        items: [
+          donateOptions[option]
+        ],
       })
     });
     if (!response.ok) {
--- greenforest-v2.0.0/server.js
+++ greenforest-v2.0.0/server.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+console.log(process.env.STRIPE_SECRET_KEY)
 const express = require('express');
 const cors = require('cors');  // Import CORS middleware
 
 const app = express();
 
 app.use(cors());  // Enable CORS
+app.use(express.json());
 
 // Replace this with your actual domain
 const YOUR_DOMAIN = 'http://127.0.0.1:5501';
 
 app.post('/create-checkout-session', async (req, res) => {
   try {
+    console.log(req.body);
+    const { items } = req.body;
+
+    // console.log(req)
     const session = await stripe.checkout.sessions.create({
       ui_mode: 'embedded',
       submit_type: 'pay',
@@ -19,10 +25,7 @@ app.post('/create-checkout-session', async (req, res) => {
       shipping_address_collection: {
         allowed_countries: ['US', 'CA'],
       },
-      line_items: [{
-        price: 'price_1QAfCMFV91ot5Yz8AoZocJmR',
-        quantity: 1
-      }],
+      line_items: items,
       mode: 'payment',
       return_url: `${YOUR_DOMAIN}/return.html?session_id={CHECKOUT_SESSION_ID}`,
     });
